4ca158a410ac4c9aa7e35a5d11aed735083c54ce
Angel Mitra <angelmitra@Angels-MacBook-Pro.local>
2026-05-29 21:42:25 +0530

    Add artwork metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
+import ArtworkCard from "../components/artwork-cards";
+import { artworks } from "@/data/artworks";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black text-white">
-      <h1 className="text-6xl p-10">
+    <main className="min-h-screen bg-black text-white p-8">
+      <h1 className="text-5xl font-bold mb-8">
         Angel's Gallery
       </h1>
+
+      <div className="grid grid-cols-3 gap-6">
+      {artworks.map((artwork) => (
+        <ArtworkCard
+          key={artwork.id}
+          title={artwork.title}
+          artist={artwork.artist}
+          image={artwork.image}
+          year={artwork.year}
+          country={artwork.country}
+        />
+      ))}
+    </div>
     </main>
-  );  
+  );
 }
---
+++ data/artworks.ts
@@ -0,0 +1,57 @@
+export const artworks = [
+  {
+    id: 1,
+    title: "The Kiss",
+    artist: "Gustav Klimt",
+    year: 1908,
+    country: "Austria",
+    category: "Painting",
+    medium: "Oil and gold leaf",
+    movement: "Art Nouveau",
+    image: "/artworks/kiss-klimt.jpg",
+    tags: ["love", "gold"],
+    personal_note: "Beautiful use of gold."
+  },
+
+  {
+    id: 2,
+    title: "The Starry Night Over the Rhône",
+    artist: "Vincent van Gogh",
+    year: 1888,
+    country: "France",
+    category: "Painting",
+    medium: "Oil",
+    movement: "Post-Impressionism",
+    image: "/artworks/starrynightoverrhone-vangogh.jpg",
+    tags: ["night", "water"],
+    personal_note: "One of my favorite night paintings."
+  },
+
+  {
+    id: 3,
+    title: "The Scream",
+    artist: "Edvard Munch",
+    year: 1893,
+    country: "Norway",
+    category: "Painting",
+    medium: "Oil, tempera and pastel",
+    movement: "Expressionism",
+    image: "/artworks/thescream-munch.jpeg",
+    tags: ["anxiety", "emotion"],
+    personal_note: "Raw emotional intensity."
+  },
+
+  {
+    id: 4,
+    title: "The Great Wave",
+    artist: "Hokusai",
+    year: 1831,
+    country: "Japan",
+    category: "Printmaking",
+    medium: "Woodblock Print",
+    movement: "Ukiyo-e",
+    image: "/artworks/thewave-hokusai.jpeg",
+    tags: ["ocean", "japan"],
+    personal_note: "The most iconic print ever made."
+  },
+];
