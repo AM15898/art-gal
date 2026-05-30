05eb4d1df1829c917eadfae558855705b9f7c259
Angel Mitra <angelmitra@Angels-MacBook-Pro.local>
2026-05-30 16:16:28 +0530

    Create artwork detail pages

---
+++ app/artwork/[id]/page.tsx
@@ -0,0 +1,43 @@
+import { artworks } from "@/data/artworks";
+import Image from "next/image";
+
+export default async function ArtworkPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const artwork = artworks.find(
+    (a) => a.id === Number(id)
+  );
+
+  if (!artwork) {
+    return <div>Artwork not found</div>;
+  }
+
+  return (
+    <main className="min-h-screen bg-black text-white p-8">
+      <div className="max-w-4xl mx-auto">
+
+        <div className="relative aspect-[4/3] mb-8">
+          <Image
+            src={artwork.image}
+            alt={artwork.title}
+            fill
+            className="object-contain"
+          />
+        </div>
+
+        <h1 className="text-4xl font-bold">
+          {artwork.title}
+        </h1>
+
+        <p className="text-zinc-400 mt-2">
+          {artwork.artist}
+        </p>
+
+      </div>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,64 @@
+"use client";
+
 import Image from "next/image";
 import ArtworkCard from "../components/artwork-cards";
 import { artworks } from "@/data/artworks";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filteredArtworks =
+  selectedCategory === "All"
+    ? artworks
+    : artworks.filter(
+        (artwork) => artwork.category === selectedCategory
+      );
+
   return (
     <main className="min-h-screen bg-black text-white p-8">
       <h1 className="text-5xl font-bold mb-8">
         Angel's Gallery
       </h1>
 
+      <div className="flex gap-3 mb-8">
+        <button
+          onClick={() => setSelectedCategory("All")}
+          className={`px-4 py-2 rounded ${
+            selectedCategory === "All"
+              ? "bg-white text-black"
+              : "bg-zinc-800 text-white"
+          }`}
+        >
+          All
+        </button>
+
+        <button
+          onClick={() => setSelectedCategory("Painting")}
+          className={`px-4 py-2 rounded ${
+            selectedCategory === "All"
+              ? "bg-white text-black"
+              : "bg-zinc-800 text-white"
+          }`}
+        >
+          Painting
+        </button>
+
+        <button
+          onClick={() => setSelectedCategory("Printmaking")}
+          className={`px-4 py-2 rounded ${
+            selectedCategory === "All"
+              ? "bg-white text-black"
+              : "bg-zinc-800 text-white"
+          }`}
+        >
+          Printmaking
+        </button>
+    </div>
+
       <div className="grid grid-cols-3 gap-6">
-      {artworks.map((artwork) => (
+      {filteredArtworks.map((artwork) => (
         <ArtworkCard
           key={artwork.id}
           title={artwork.title}
